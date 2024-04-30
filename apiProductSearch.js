24542d603cfe54f6c6ba8bb2e38844d898542b6e
igo4ek <babka2002@ya.ru>
2024-04-30 23:12:48 +0300

    добавлен код для тестирования API

---
+++ apiProductSearch.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const url = `http://localhost:3000/graphql`
+function searchProductByName(searchTerm) {
+    const query = `
+    query FindProductByTitleBGU($searchTerm: String!) {
+        products(where: { title: { contains: $searchTerm } }) {
+          id
+          title
+          nutrients(where: { nutrient: { is: {name:{in: ["Белки", "Жиры", "Углеводы"]}} }}) {
+            valueString
+            valueAmount
+            valueExponent
+            nutrient {
+              name
+            } 
+          }
+        }
+      }
+    `;
+    const variables = {
+        searchTerm: searchTerm
+    };
+    axios.post(url, {
+        query: query,
+        variables: variables
+    })
+    .then(response => {
+        let { products } = response.data.data;
+        console.log(products);
+    })
+    .catch(error => {
+        console.error('Error fetching product:', error);
+    });
+}
+searchProductByName('яйцо');  // Пример поиска продукта
